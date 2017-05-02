f7edd3e437feb6a9a1b060fb542af0f5b5a8913f
albert <albert.sisfountan@gmail.com>
2017-05-02 18:29:37 +0700

    Belajar index

---
+++ belajar/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sisfo Untan</title>
+</head>
+<body>
+
+	<h1>Sistem Informasi</h1>
+
+<?php
+include_once 'table.php';
+require_once 'satu.php';
+
+?>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo hic non tenetur magni! Quibusdam impedit voluptates dolor quae, tenetur, nobis blanditiis reiciendis deserunt, fuga ea architecto deleniti beatae soluta totam.</p>
+	<?php
+include_once 'table.php';
+	?>
+</body>
+</html>
